9263297a305b5169ecd6a423274b5eda0d2a175a
bslater-c7ks7s <bslater@clockwork-solutions.com>
2017-12-19 03:58:13 -0600

    add even more unit tests

---
+++ src/test/java/com/spothero/interview/pojo/RateIntervalTest.java
@@ -0,0 +1,42 @@
+package com.spothero.interview.pojo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * RateInterval Tester.
+ *
+ * @author Brian Slater
+ * @version 1.0
+ * @since <pre>Dec 19, 2017</pre>
+ */
+public class RateIntervalTest {
+
+    RateInterval rateInterval;
+
+    @Before
+    public void before() throws Exception {
+        Interval interval = new Interval("1000-1100");
+        this.rateInterval = new RateInterval(interval, 1000);
+    }
+
+    /**
+     * Method: getInterval()
+     */
+    @Test
+    public void testGetInterval() throws Exception {
+        Interval expectedInterval = new Interval("1000-1100");
+        Assert.assertEquals(expectedInterval, this.rateInterval.getInterval());
+    }
+
+    /**
+     * Method: getRate()
+     */
+    @Test
+    public void testGetRate() throws Exception {
+        Assert.assertEquals(1000, this.rateInterval.getRate());
+    }
+
+
+}
---
+++ src/test/java/com/spothero/interview/util/RateUtilsTest.java
@@ -0,0 +1,55 @@
+package com.spothero.interview.util;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * RateUtils Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>Dec 19, 2017</pre>
+ * @version 1.0
+ */
+public class RateUtilsTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     *
+     * Method: partitionRatesByDay(Rate originalRate, Map<String, List<RateInterval>> dayMap)
+     *
+     */
+    @Test
+    public void testPartitionRatesByDay() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: buildRateIntervalListFromOrignalRate(Rate originalRate)
+     *
+     */
+    @Test
+    public void testBuildRateIntervalListFromOrignalRate() throws Exception {
+//TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: findRateforRequestedDateTime(RateEntity rateEntity, String startInterval, String endInterval)
+     *
+     */
+    @Test
+    public void testFindRateforRequestedDateTime() throws Exception {
+//TODO: Test goes here...
+    }
+
+
+}
